25561af1ce5f57cca22926014ce49e38b080e9b6
manishpayak <mpayak35@gmail.com>
2024-01-23 23:39:40 +0530

    backand

---
+++ ram.js
@@ -0,0 +1,20 @@
+// document.write("jayshree ram<br>");
+// document.write("<i>jayshree ram<br></i>");
+// document.write("jayshree ram");
+
+// use of veriable
+// var m ="ramram"
+// m = "row"
+// var n = 100;
+// n = 20;
+// document.write(m);
+// document.write(n);
+
+// var riderman = "oye"
+// document.write(riderman);
+
+// lat variable
+
+
+
+
